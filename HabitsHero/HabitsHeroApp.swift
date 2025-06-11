1265d4e678bc4e08c60e68b32ab0f65bacd04065
Marcos Contente <contente.marcos@gmail.com>
2025-06-11 11:16:59 -0300

    Make project

---
+++ HabitsHero/HabitsHeroApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitsHeroApp.swift
+//  HabitsHero
+//
+//  Created by Marcos Contente on 11/06/25.
+//
+
+import SwiftUI
+
+@main
+struct HabitsHeroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
